79ca6557bd0dcaff19fd217899f44d9aaa9ba4ef
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-09-21 12:53:44 +0300

    added hashmap and min heap

--- data_struct.py
+++ data_struct.py
@@ -2,7 +2,7 @@
 This library nade for people that are learning data structures, to help them if they face any problems
 """"
 #th
-
+import sys
 
 # Linked lists Node class 
 class Node:
@@ -452,8 +452,135 @@ class Graph:
         
     return False
 
-class Hash_map:
-  pass
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.hash_table = [[] for _ in range(self.size)]
+  
+    def set_val(self, key, val):
+        hashed_key = hash(key) % self.size
+        bucket = self.hash_table[hashed_key]
+  
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+              
+            if record_key == key:
+                found_key = True
+                break
 
-class heaps:
-  pass
+        if found_key:
+            bucket[index] = (key, val)
+        else:
+            bucket.append((key, val))
+
+    def get_val(self, key):
+
+        hashed_key = hash(key) % self.size
+          
+        bucket = self.hash_table[hashed_key]
+  
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+              
+            if record_key == key:
+                found_key = True
+                break
+  
+        if found_key:
+            return record_val
+        else:
+            return "No record found"
+    def delete_val(self, key):
+
+        hashed_key = hash(key) % self.size
+        bucket = self.hash_table[hashed_key]
+  
+        found_key = False
+        for index, record in enumerate(bucket):
+            record_key, record_val = record
+
+            if record_key == key:
+                found_key = True
+                break
+        if found_key:
+            bucket.pop(index)
+        return
+
+    def __str__(self):
+        return "".join(str(item) for item in self.hash_table)
+
+
+class MinHeap:
+     
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.size = 0
+        self.Heap = [0]*(self.maxsize + 1)
+        self.Heap[0] = -1 * sys.maxsize
+        self.FRONT = 1
+ 
+    def parent(self, pos):
+        return pos//2
+ 
+    def leftChild(self, pos):
+        return 2 * pos
+ 
+    def rightChild(self, pos):
+        return (2 * pos) + 1
+ 
+    def isLeaf(self, pos):
+        if pos >= (self.size//2) and pos <= self.size:
+            return True
+        return False
+ 
+    def swap(self, fpos, spos):
+        self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos]
+
+    def minHeapify(self, pos):
+
+        if not self.isLeaf(pos):
+            if (self.Heap[pos] > self.Heap[self.leftChild(pos)] or
+               self.Heap[pos] > self.Heap[self.rightChild(pos)]):
+ 
+                if self.Heap[self.leftChild(pos)] < self.Heap[self.rightChild(pos)]:
+                    self.swap(pos, self.leftChild(pos))
+                    self.minHeapify(self.leftChild(pos))
+
+                else:
+                    self.swap(pos, self.rightChild(pos))
+                    self.minHeapify(self.rightChild(pos))
+
+    def insert(self, element):
+        if self.size >= self.maxsize :
+            return
+        self.size+= 1
+        self.Heap[self.size] = element
+ 
+        current = self.size
+ 
+        while self.Heap[current] < self.Heap[self.parent(current)]:
+            self.swap(current, self.parent(current))
+            current = self.parent(current)
+ 
+    def Print(self):
+        for i in range(1, (self.size//2)+1):
+            print(" PARENT : "+ str(self.Heap[i])+" LEFT CHILD : "+
+                                str(self.Heap[2 * i])+" RIGHT CHILD : "+
+                                str(self.Heap[2 * i + 1]))
+ 
+
+    def minHeap(self):
+ 
+        for pos in range(self.size//2, 0, -1):
+            self.minHeapify(pos)
+ 
+
+    def remove(self):
+ 
+        popped = self.Heap[self.FRONT]
+        self.Heap[self.FRONT] = self.Heap[self.size]
+        self.size-= 1
+        self.minHeapify(self.FRONT)
+        return popped
